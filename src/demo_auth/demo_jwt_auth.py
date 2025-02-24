198197def6db703b756c619d5dd57b520dcba04d
Pavlov Danil <danpavkzn@mail.ru>
2025-02-24 21:49:23 +0300

    added проврека на тип токена.чтобы был только доступ к сайту по access а не по любому

--- src/demo_auth/demo_jwt_auth.py
+++ src/demo_auth/demo_jwt_auth.py
@@ -1,14 +1,21 @@
 from fastapi import APIRouter, Depends, Form, HTTPException, status
 from fastapi.security import (
     OAuth2PasswordBearer,
+    HTTPBearer,
 )
 from src.api.schemas.schemas import UserSchemaTest
 from src.auth import utils as auth_utils
 from pydantic import BaseModel
 from jwt import InvalidTokenError
 
-from src.demo_auth.helpers import create_access_token, create_refresh_token
+from src.demo_auth.helpers import (
+    ACCESS_TOKEN_TYPE,
+    TOKEN_TYPE_FIELD,
+    create_access_token,
+    create_refresh_token,
+)
 
+http_bearer = HTTPBearer(auto_error=False)
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/jwt/login")
 
 
@@ -18,7 +25,7 @@ class TokenInfo(BaseModel):
     token_type: str = "Bearer"
 
 
-router = APIRouter(prefix="/jwt", tags=["JWT"])
+router = APIRouter(prefix="/jwt", tags=["JWT"], dependencies=[Depends(http_bearer)])
 
 john = UserSchemaTest(
     username="john",
@@ -86,6 +93,13 @@ def get_current_token_payload(  # функция для получения paylo
 def get_current_auth_user(
     payload: dict = Depends(get_current_token_payload),
 ) -> UserSchemaTest:  # Получаем юзера находя
+    token_type = payload.get(TOKEN_TYPE_FIELD)
+    if token_type != ACCESS_TOKEN_TYPE:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"token invalid type {token_type!r} expected {ACCESS_TOKEN_TYPE!r}",
+        )
+
     username: str | None = payload.get(
         "username"
     )  # Его username в токене и берем от туда sub(то есть о ком вообще речь.
